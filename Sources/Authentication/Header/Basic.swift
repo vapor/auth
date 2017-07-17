9f6608b9527f1e656e9e1a9939a181b2053b6c06
cardoso <matheusmcrds@gmail.com>
2017-07-17 20:52:16 -0300

    Add AuthorizationHeader initializers for Basic and Bearer

--- Sources/Authentication/Header/Basic.swift
+++ Sources/Authentication/Header/Basic.swift
@@ -1,21 +1,25 @@
-import Core
-
 extension AuthorizationHeader {
     public var basic: Password? {
         guard let range = string.range(of: "Basic ") else {
             return nil
         }
-
+        
         let token = string.substring(from: range.upperBound)
-
+        
         let decodedToken = token.makeBytes().base64Decoded.makeString()
         guard let separatorRange = decodedToken.range(of: ":") else {
             return nil
         }
-
+        
         let username = decodedToken.substring(to: separatorRange.lowerBound)
         let password = decodedToken.substring(from: separatorRange.upperBound)
-
+        
         return Password(username: username, password: password)
+    }
+    
+    public init(basic: Password) {
+        let credentials = "\(basic.username):\(basic.password)"
+        let encoded = credentials.makeBytes().base64Encoded.makeString()
+        self.init(string: "Basic \(encoded)")
     }
 }
--- Sources/Authentication/Header/Bearer.swift
+++ Sources/Authentication/Header/Bearer.swift
@@ -3,8 +3,12 @@ extension AuthorizationHeader {
         guard let range = string.range(of: "Bearer ") else {
             return nil
         }
-
+        
         let token = string.substring(from: range.upperBound)
         return Token(string: token)
+    }
+    
+    public init(bearer: Token) {
+        self.init(string: "Bearer \(bearer.string)")
     }
 }
