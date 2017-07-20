f53b922636012ba0fdeb4aa375b639fc0c251dbd
cardoso <matheusmcrds@gmail.com>
2017-07-19 21:52:15 -0300

    Fix spacing

--- Sources/Authentication/Header/Basic.swift
+++ Sources/Authentication/Header/Basic.swift
@@ -3,20 +3,20 @@ extension AuthorizationHeader {
         guard let range = string.range(of: "Basic ") else {
             return nil
         }
-        
+
         let token = string.substring(from: range.upperBound)
-        
+
         let decodedToken = token.makeBytes().base64Decoded.makeString()
         guard let separatorRange = decodedToken.range(of: ":") else {
             return nil
         }
-        
+
         let username = decodedToken.substring(to: separatorRange.lowerBound)
         let password = decodedToken.substring(from: separatorRange.upperBound)
-        
+
         return Password(username: username, password: password)
     }
-    
+
     public init(basic: Password) {
         let credentials = "\(basic.username):\(basic.password)"
         let encoded = credentials.makeBytes().base64Encoded.makeString()
