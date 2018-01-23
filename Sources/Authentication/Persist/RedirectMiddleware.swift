b9b4016be6d191d1c6dc627309cd4a9cc3f1de63
Tim <0xtimc@gmail.com>
2018-01-23 13:37:14 +0000

    Remove warning and update middleware with comments

--- Sources/Authentication/Persist/RedirectMiddleware.swift
+++ Sources/Authentication/Persist/RedirectMiddleware.swift
@@ -1,13 +1,23 @@
 import Vapor
 
+/// Basic middleware to redirect unauthenticated requests to the supplied path
 public struct RedirectMiddleware<A>: Middleware where A: Authenticatable {
 
+    /// The path to redirect to
     let path: String
 
-    public init(A authenticableType: A.Type = A.self, path: String) {
+    /**
+     Initialise the `RedirectMiddleware`
+
+     - parameters:
+        - authenticatableType: The type to check authentication against
+        - path: The path to redirect to if the request is not authenticated
+     */
+    public init(A authenticatableType: A.Type = A.self, path: String) {
         self.path = path
     }
 
+    /// See Middleware.respond
     public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
         if try request.isAuthenticated(A.self) {
             return try next.respond(to: request)
