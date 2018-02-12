e18042f2f5a30da043cb49ff3683878f392fa0a1
Tanner Nelson <me@tanner.xyz>
2018-02-12 12:14:17 -0500

    3.0 beta deps

--- Package.swift
+++ Package.swift
@@ -8,25 +8,25 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
         
         // Cryptography modules
-        .package(url: "https://github.com/vapor/crypto.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/crypto.git", .exact("3.0.0-beta.1")),
 
         // Non-blocking networking for Swift (HTTP and WebSockets).
-        .package(url: "https://github.com/vapor/engine.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/engine.git", .exact("3.0.0-beta.1")),
 
         // Swift ORM (queries, models, and relations) for NoSQL and SQL databases.
-        .package(url: "https://github.com/vapor/fluent.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/fluent.git", .exact("3.0.0-beta.1")),
 
         // Service container and configuration system.
-        .package(url: "https://github.com/vapor/service.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/service.git", .exact("1.0.0-beta.1")),
 
         // A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-beta.1")),
     ],
     targets: [
         .target(name: "Authentication", dependencies: ["Async", "Bits", "Crypto", "Debugging", "Fluent", "HTTP", "Service", "Vapor"]),
