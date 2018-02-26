aef0d5736eb36f8828d62634793abcd87d5da40c
Tanner Nelson <me@tanner.xyz>
2018-02-26 14:20:41 -0500

    metadata updates

--- Package.swift
+++ Package.swift
@@ -7,26 +7,26 @@ let package = Package(
         .library(name: "Authentication", targets: ["Authentication"]),
     ],
     dependencies: [
-        // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
+        // ⏱ Promises and reactive-streams in Swift built for high-performance and scalability.
+        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
 
-        // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
+        // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
         
-        // Cryptography modules
-        .package(url: "https://github.com/vapor/crypto.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
+        // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-rc"),
 
-        // Non-blocking networking for Swift (HTTP and WebSockets).
-        .package(url: "https://github.com/vapor/engine.git", "3.0.0-beta.1"..<"3.0.0-beta.3"),
+        // 🚀 Non-blocking, event-driven networking for Swift (HTTP and WebSockets).
+        .package(url: "https://github.com/vapor/engine.git", from: "3.0.0-rc"),
 
-        // Swift ORM (queries, models, and relations) for NoSQL and SQL databases.
-        .package(url: "https://github.com/vapor/fluent.git", "3.0.0-beta.1"..<"3.0.0-beta.4"),
+        // 🖋 Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
+        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc"),
 
-        // Service container and configuration system.
-        .package(url: "https://github.com/vapor/service.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
+        // 📦 Dependency injection / inversion of control framework.
+        .package(url: "https://github.com/vapor/service.git", from: "1.0.0-rc"),
 
-        // A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", "3.0.0-beta.1"..<"3.0.0-beta.4"),
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
     ],
     targets: [
         .target(name: "Authentication", dependencies: ["Async", "Bits", "Crypto", "Debugging", "Fluent", "HTTP", "Service", "Vapor"]),
