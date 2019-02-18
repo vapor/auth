ed402783942af1d40b5c9e537e956aea51618f0e
Tim <0xTim@users.noreply.github.com>
2019-02-18 20:28:27 +0000

    Set the correct supported Vapor version in the manifest

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
 
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.2.0"),
         
         // Fluent SQLite, only for testing.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
